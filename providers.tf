c6f67e6ca5a703fff9ce776e3f94971819ff5a59
Enio Yao Lee <enioyao@gmail.com>
2023-11-29 16:10:35 -0300

    update google version

--- providers.tf
+++ providers.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.54.0"
+      version = "5.7.0"
     }
   }
 }
 
-# Configura o Provider Google Cloud com o Projeto
 provider "google" {
   project = var.project_id
 }
